4025ca80f2b804fd389fda3c36eedb9b5c61a6aa
Reece Williams <reecepbcups@gmail.com>
2024-02-10 15:11:17 -0600

    check for admin mint if inflation is >0

---
+++ app/decorators/inflation_disable_minting.go
@@ -0,0 +1,51 @@
+package decorators
+
+import (
+	"context"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	manifestkeeper "github.com/liftedinit/manifest-ledger/x/manifest/keeper"
+	tokenfactorytypes "github.com/reecepbcups/tokenfactory/x/tokenfactory/types"
+)
+
+type IsSudoAdminFunc func(ctx context.Context, fromAddr string) bool
+
+type MsgManualMintFilterDecorator struct {
+	mk              *manifestkeeper.Keeper
+	isSudoAdminFunc IsSudoAdminFunc
+}
+
+func NewMsgManualMintFilterDecorator(mk *manifestkeeper.Keeper, isSudoAdminFunc IsSudoAdminFunc) MsgManualMintFilterDecorator {
+	return MsgManualMintFilterDecorator{
+		mk:              mk,
+		isSudoAdminFunc: isSudoAdminFunc,
+	}
+}
+
+func (mfd MsgManualMintFilterDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (newCtx sdk.Context, err error) {
+	// iterate all messages, see if any are a tokenfactory message from a sudo admin.
+	// if there is and inflation is current >0%, return an error
+	if err := mfd.hasInvalidTokenFactoryMsg(ctx, tx.GetMsgs()); err != nil {
+		return ctx, err
+	}
+
+	return next(ctx, tx, simulate)
+}
+
+func (mfd MsgManualMintFilterDecorator) hasInvalidTokenFactoryMsg(ctx sdk.Context, msgs []sdk.Msg) error {
+	for _, msg := range msgs {
+		if m, ok := msg.(*tokenfactorytypes.MsgMint); ok {
+			if mfd.isSudoAdminFunc(ctx, m.Sender) {
+				isInflationEnabled := mfd.mk.IsManualMintingEnabled(ctx)
+				if isInflationEnabled != nil {
+					return isInflationEnabled
+				}
+			}
+
+			return nil
+		}
+	}
+
+	return nil
+}
