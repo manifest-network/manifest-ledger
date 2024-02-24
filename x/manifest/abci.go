03725d07af5ec0a8014302ff1f3e959e946b562d
Reece Williams <reecepbcups@gmail.com>
2024-02-24 10:55:38 -0600

    guard clauses

--- x/manifest/abci.go
+++ x/manifest/abci.go
@@ -23,21 +23,29 @@ func BeginBlocker(ctx context.Context, k keeper.Keeper, mk mintkeeper.Keeper, bk
 		return err
 	}
 
+	// If there is no one to pay out, skip
+	if len(params.StakeHolders) == 0 {
+		return nil
+	}
+
+	// Calculate the per block inflation rewards to pay out in coins
 	mintedCoin := k.BlockRewardsProvision(ctx, params.Inflation.MintDenom)
 	mintedCoins := sdk.NewCoins(mintedCoin)
 
+	// If no inflation payout this block, skip
+	if mintedCoin.IsZero() {
+		return nil
+	}
+
+	// mint the tokens to the network
 	if err := mk.MintCoins(ctx, mintedCoins); err != nil {
 		return err
 	}
 
+	// Payout all the stakeholders with their respective share of the minted coins
 	if err := k.PayoutStakeholders(ctx, mintedCoin); err != nil {
 		return err
 	}
-
-	// send the minted coins to the fee collector account
-	// if err := mk.AddCollectedFees(ctx, mintedCoins); err != nil {
-	// 	return err
-	// }
 
 	if mintedCoin.Amount.IsInt64() {
 		defer telemetry.ModuleSetGauge(minttypes.ModuleName, float32(mintedCoin.Amount.Int64()), "minted_tokens")
