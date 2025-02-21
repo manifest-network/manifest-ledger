e154359a11eb84530ca82f604c69420cb5e800b1
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2025-02-21 10:02:27 -0500

    fix: upgrade to noop

--- app/upgrades/next/upgrades.go
+++ app/upgrades/next/upgrades.go
@@ -3,15 +3,11 @@ package next
 import (
 	"context"
 
-	errorsmod "cosmossdk.io/errors"
 	storetypes "cosmossdk.io/store/types"
 	upgradetypes "cosmossdk.io/x/upgrade/types"
 
 	"github.com/cosmos/cosmos-sdk/types/module"
 
-	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
-
-	"github.com/liftedinit/manifest-ledger/app/helpers"
 	"github.com/liftedinit/manifest-ledger/app/upgrades"
 )
 
@@ -20,9 +16,7 @@ func NewUpgrade(name string) upgrades.Upgrade {
 		UpgradeName:          name,
 		CreateUpgradeHandler: CreateUpgradeHandler,
 		StoreUpgrades: storetypes.StoreUpgrades{
-			Added: []string{
-				wasmtypes.ModuleName,
-			},
+			Added:   []string{},
 			Deleted: []string{},
 		},
 	}
@@ -31,26 +25,9 @@ func NewUpgrade(name string) upgrades.Upgrade {
 func CreateUpgradeHandler(
 	mm *module.Manager,
 	configurator module.Configurator,
-	keepers *upgrades.AppKeepers,
+	_ *upgrades.AppKeepers,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx context.Context, _ upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-		fromVM, err := mm.RunMigrations(ctx, configurator, fromVM)
-		if err != nil {
-			return fromVM, err
-		}
-
-		// Set CosmWasm params
-		wasmParams := wasmtypes.DefaultParams()
-		wasmParams.CodeUploadAccess = wasmtypes.AccessConfig{
-			Permission: wasmtypes.AccessTypeAnyOfAddresses,
-			Addresses:  []string{helpers.GetPoAAdmin()},
-		}
-		wasmParams.InstantiateDefaultPermission = wasmtypes.AccessTypeAnyOfAddresses
-
-		if err := keepers.WasmKeeper.SetParams(ctx, wasmParams); err != nil {
-			return fromVM, errorsmod.Wrapf(err, "unable to set CosmWasm params")
-		}
-
-		return fromVM, nil
+		return mm.RunMigrations(ctx, configurator, fromVM)
 	}
 }
