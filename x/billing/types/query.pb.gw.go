a943508cc9408cb5968af0f72c6a901629f2a1cb
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2025-12-08 14:05:35 -0500

    chore(billing): fix proto

--- x/billing/types/query.pb.gw.go
+++ x/billing/types/query.pb.gw.go
@@ -214,7 +214,7 @@ func local_request_Query_LeasesByTenant_0(ctx context.Context, marshaler runtime
 }
 
 var (
-	filter_Query_LeasesByProvider_0 = &utilities.DoubleArray{Encoding: map[string]int{"provider": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
+	filter_Query_LeasesByProvider_0 = &utilities.DoubleArray{Encoding: map[string]int{"provider_id": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
 )
 
 func request_Query_LeasesByProvider_0(ctx context.Context, marshaler runtime.Marshaler, client QueryClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
@@ -228,15 +228,15 @@ func request_Query_LeasesByProvider_0(ctx context.Context, marshaler runtime.Mar
 		_   = err
 	)
 
-	val, ok = pathParams["provider"]
+	val, ok = pathParams["provider_id"]
 	if !ok {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "provider")
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "provider_id")
 	}
 
-	protoReq.Provider, err = runtime.String(val)
+	protoReq.ProviderId, err = runtime.Uint64(val)
 
 	if err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "provider", err)
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "provider_id", err)
 	}
 
 	if err := req.ParseForm(); err != nil {
@@ -262,15 +262,15 @@ func local_request_Query_LeasesByProvider_0(ctx context.Context, marshaler runti
 		_   = err
 	)
 
-	val, ok = pathParams["provider"]
+	val, ok = pathParams["provider_id"]
 	if !ok {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "provider")
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "provider_id")
 	}
 
-	protoReq.Provider, err = runtime.String(val)
+	protoReq.ProviderId, err = runtime.Uint64(val)
 
 	if err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "provider", err)
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "provider_id", err)
 	}
 
 	if err := req.ParseForm(); err != nil {
@@ -850,7 +850,7 @@ var (
 
 	pattern_Query_LeasesByTenant_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4, 1, 0, 4, 1, 5, 4}, []string{"liftedinit", "billing", "v1", "leases", "tenant"}, "", runtime.AssumeColonVerbOpt(false)))
 
-	pattern_Query_LeasesByProvider_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4, 1, 0, 4, 1, 5, 4}, []string{"liftedinit", "billing", "v1", "leases", "provider"}, "", runtime.AssumeColonVerbOpt(false)))
+	pattern_Query_LeasesByProvider_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4, 1, 0, 4, 1, 5, 5}, []string{"liftedinit", "billing", "v1", "leases", "provider", "provider_id"}, "", runtime.AssumeColonVerbOpt(false)))
 
 	pattern_Query_CreditAccount_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"liftedinit", "billing", "v1", "credit", "tenant"}, "", runtime.AssumeColonVerbOpt(false)))
 
